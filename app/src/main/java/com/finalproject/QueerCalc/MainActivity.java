348b0e7b28fcfe53b13d4bdfc3074a5b7f250f65
braxton hancock <bdhancock@olivet.edu>
2021-11-18 15:20:20 -0600

    first commit

---
+++ app/src/main/java/com/finalproject/QueerCalc/MainActivity.java
@@ -0,0 +1,29 @@
+package com.finalproject.QueerCalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.finalproject.QueerCalc.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+
+        String surName = getColoredSpanned("Patel","#000080");
+
+    }
+
+    private String getColoredSpanned(String text, String color) {
+        String input = "<font color=" + color + ">" + text + "</font>";
+        return input;
+    }
+}
---
+++ app/src/main/java/com/finalproject/QueerCalc/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.finalproject.QueerCalc.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
